a69ef7ef8348217f22aef99ef58ef7bcfd33c6ea
durgeshru <durgeshkumar7433@gmail.com>
2026-05-12 23:25:57 +0530

    update

--- src/component/Section2.jsx
+++ src/component/Section2.jsx
@@ -1,7 +1,11 @@
 function Section2() {
   return (
     <section style={{height: "100vh", display: "flex", alignItems: "center", justifyContent: "center", color: "white", fontSize: "2rem"}}>
-      <h1>Section 2</h1>
+ <div>
+  <h2>
+    hey i am durgesh and i just complete my bca from rajasthan university . i have 1 year of experience in react node and also working with 3d programing tool.
+  </h2>
+ </div>
     </section>
   )
 }
